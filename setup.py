0584ee9af8440011dc13514ea8bf3fb1b6e51afb
unknown <you@example.com>
2024-02-18 01:26:18 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return list
+    '''
+    with open(file_path) as f:
+        requirements = f.read().splitlines()
+        requirements = [req for req in requirements if '-e .' not in req]
+
+    return requirements
+
+ 
+    
+
+
+setup(
+
+    name='MLPROJECT-GENERIC',
+    version='0.0.1',
+    author='Hassan Taj',
+    author_email='hassantaj00@gmail.com',
+    packages= find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
